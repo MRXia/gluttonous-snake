c6b49d2ba1610c0a939f18579e4f2d91f9064f96
xiazijian <zijian.xia@mail.dealmoon.com>
2018-05-29 10:36:44 +0800

    feat(module) create project and create Point class

---
+++ src/com/mrxia/snake/Main.java
@@ -0,0 +1,8 @@
+package com.mrxia.snake;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
---
+++ src/com/mrxia/snake/module/Point.java
@@ -0,0 +1,67 @@
+package com.mrxia.snake.module;
+
+/**
+ * 坐标点类
+ * @author xiazijian
+ */
+public class Point {
+
+    private int x;
+
+    private int y;
+
+    private int hash;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    @Override
+    public int hashCode() {
+        int h = hash;
+        if (h == 0) {
+            int[] val = {x, y};
+
+            for (int v : val) {
+                h = 31 * h + v;
+            }
+            hash = h;
+        }
+        return h;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+
+        if (this == obj) {
+            return true;
+        }
+        if (obj instanceof Point) {
+            Point otherPoint = (Point) obj;
+            return otherPoint.getX() == this.x && otherPoint.getY() == this.y;
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return "Point [x=" + x + ", y=" + y + "]";
+    }
+}
